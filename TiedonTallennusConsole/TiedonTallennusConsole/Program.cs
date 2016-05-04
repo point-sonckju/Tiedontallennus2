531c4c438ed69efb0ceb79b0532b51105705115a
Juha Sonck <juha.sonck@pointcollege.fi>
2016-05-04 16:21:06 +0300

    Viimeistelty sovelluksen toimintaa

--- TiedonTallennusConsole/TiedonTallennusConsole/Program.cs
+++ TiedonTallennusConsole/TiedonTallennusConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,29 @@ namespace TiedonTallennusConsole
     {
         static void Main(string[] args)
         {
+            string tiedosto = "C:\\Temp\\Lämpötila.txt"; //tai @"C:\Temp\Lämpötila.txt";
+            //Luetaan aiemmin asetettu lämpötila
+            if (File.Exists(tiedosto))
+            {
+                string aiempilämpötila = File.ReadAllText(tiedosto);
+                Console.WriteLine("Aiemmin asetettu lämpötila oli: " + aiempilämpötila + " astetta");
+            }
+            else
+            {
+                Console.Write("Tervetuloa sovellukseen - ");
+            }
+
+            int lämpötila = 0;
+            Console.Write("Anna uusi lämpötila: ");
+            string syöte = Console.ReadLine();
+            lämpötila = int.Parse(syöte);
+            //Tiedostoon
+
+            File.WriteAllText(tiedosto, lämpötila.ToString());
+            Console.WriteLine("Uusi lämpötila on tallennettu!");
+            Console.ReadLine();
+
+
         }
     }
 }
